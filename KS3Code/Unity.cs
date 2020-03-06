c1160a8995d1a84db9a42348b0d546c54b650d55
Jesus Rodriguez Valencia <jesus.rodriguez.valencia@gmail.com>
2020-03-06 20:57:01 +0100

    recompiling for KSP 1.9

--- KS3Code/Unity.cs
+++ KS3Code/Unity.cs
@@ -4570,12 +4570,7 @@ namespace KS3P
 
         public bool isHdr
         {
-            // No UNITY_5_6_OR_NEWER defined in early betas of 5.6
-#if UNITY_5_6 || UNITY_5_6_OR_NEWER
             get { return camera.allowHDR; }
-#else
-            get { return camera.hdr; }
-#endif
         }
 
         public int width
